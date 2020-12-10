da25a57e48eb6064678cd1886bf38716fea7e651
Pyrus-vagus <73356536+Pyrus-vagus@users.noreply.github.com>
2020-12-10 22:42:08 +0300

    add js files

---
+++ Lesson_8/HomeWork/Grusha.js
@@ -0,0 +1,20 @@
+var head = {
+    glasses: 1
+  };
+  var table = {
+    pen: 3
+  };
+  var bed = {
+    sheet: 1,
+    pillow: 2
+  };
+  var pockets = {
+    money: 2000
+  };
+table.__proto__=head;
+bed.__proto__=table;
+pockets.__proto__=bed;  
+//console.log(pockets.pen);
+//console.log(bed.glasses);
+//console.log(table.money);
+console.log(pockets.glasses);
